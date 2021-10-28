c6ed530869cb009f93e99a3f323698cd0b88b7f5
varshaelza <varshaelza@gmail.com>
2021-10-28 02:05:35 -0400

    delete functions updated

--- src/app/admin/components/manage-books/manage-books.component.ts
+++ src/app/admin/components/manage-books/manage-books.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 
@@ -9,20 +10,80 @@ import { AdminService } from '../../services/admin.service';
 })
 export class ManageBooksComponent implements OnInit {
   books:any;
-  lastpos:any;
+
   constructor(private adminService:AdminService) { }
 
   ngOnInit(): void {
-    console.log("submitting");
+    console.log("Loading");
     this.adminService.getBooks()
   .subscribe( (res:any) =>{
     console.log(res)
     this.books=res;
-    this.lastpos=this.books.at(-1).bookPosition;
+    console.log(this.books.length)
     
     
   })
 
   }
+
+  moveBookup(book:any)
+  {
+
+
+    console.log("Updating Book Position");
+    let shiftid=this.books[this.books.indexOf(book)-1].bookId
+    let shiftpos=this.books[this.books.indexOf(book)-1].bookPosition
+    console.log(this.books.indexOf(book)-1)
+    this.adminService.updateBookbyPos(book.bookId,book.bookPosition-1)
+    .subscribe( (res:any) =>{
+      console.log(res)
+      
+    })
+    this.adminService.updateBookbyPos(shiftid,shiftpos+1)
+  .subscribe( (res:any) =>{
+    console.log(res)
+    this.books=res;
+    
+    
+
+    
+  })
+  }
+
+  moveBookdown(book:any)
+  {
+    console.log("Updating Book Position");
+    let shiftid=this.books[this.books.indexOf(book)+1].bookId
+    let shiftpos=this.books[this.books.indexOf(book)+1].bookPosition
+    console.log(this.books.indexOf(book)+1)
+    this.adminService.updateBookbyPos(book.bookId,book.bookPosition+1)
+    .subscribe( (res:any) =>{
+      console.log(res)      
+    })
+    this.adminService.updateBookbyPos(shiftid,shiftpos-1)
+  .subscribe( (res:any) =>{
+    console.log(res)
+    this.books=res;
+    
+    
+
+    
+  })
+
+}
+deleteBook(book:any)
+{
+  
+  this.adminService.deleteBook(book.bookId)
+  .subscribe( (res:any) =>{
+   if(res.find((i:any)=>i.bookId===book.bookId)!=null)
+   {alert('Unable to delete :Book in use')}
+
+  this.books=res
+  
+})
+
+
+}
 
 }
--- src/app/admin/services/admin.service.ts
+++ src/app/admin/services/admin.service.ts
@@ -16,7 +16,24 @@ export class AdminService {
       return res;
       }))
   }
+  updateBookbyPos(bookId:any,bookPosition:any)
+  {
+    return this.http.put(`https://localhost:44346/Api/Books?p_bookId=${bookId}&p_bookpos=${bookPosition}`,bookId)
+    .pipe( map((res:any)=>{
+      console.log(res);
+      return res;
+      }))
+  }
 
+  deleteBook(bookId:any)
+  {
+    return this.http.delete(`https://localhost:44346/Api/Books?p_bookID=${bookId}`)
+    .pipe( map((res:any)=>{
+      // console.log(res.indexOf(cat));
+      
+      return res;
+      }))
+  } 
   getCategories()
   {
     return this.http.get('https://localhost:44346/Api/Category')
@@ -26,6 +43,24 @@ export class AdminService {
       }))
   }
 
+  updateCategorybyPos(catId:any,catPosition:any)
+  {
+    return this.http.put(`https://localhost:44346/Api/Category?id=${catId}&pos=${catPosition}`,catId)
+    .pipe( map((res:any)=>{
+      console.log(res);
+      return res;
+      }))
+  }
+
+  deleteCategory(catId:any)
+  {
+    return this.http.delete(`https://localhost:44346/Api/Category?id=${catId}`)
+    .pipe( map((res:any)=>{
+      // console.log(res.indexOf(cat));
+      
+      return res;
+      }))
+  }
   getDiscounts()
   {
     return this.http.get('https://localhost:44346/Api/Discount')
