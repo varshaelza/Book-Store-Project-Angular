ef40e0970b1ae48135acfbcca29ad6e211edc289
Ayshahananv <ayshahananvid@gmail.com>
2021-10-31 18:44:35 -0400

    updated checkout and home display

--- src/app/shopping/components/checkout/checkout.component.ts
+++ src/app/shopping/components/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,10 +11,79 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class CheckoutComponent implements OnInit {
+  currentUser: any;
+  loggedInUser:any;
+  newAddress:any;
+  //loggedIndefaultaddress:any;
+  discountObj:any;
+  discountId:any=6; //default discount Id of default coupon code
+  orderObj:any={
+    userId:JSON.parse(localStorage.getItem('authToken') || '{}'),
+    couponId:this.discountId,
+    totalAmt:0,
+    datetimeOrder:''
+  }
 
-  constructor() { }
+  constructor(private checkoutService: CartService, private activatedRoute: ActivatedRoute,private router: Router) { }
+  
+  updateAddressform = new FormGroup({
+    userAddress : new FormControl('')
+  });
+
+  couponRedeemForm= new FormGroup({
+    couponCode: new FormControl('')
+  })
+
+
 
   ngOnInit(): void {
+    this.currentUser = JSON.parse(localStorage.getItem('authToken') || '{}');
+    this.getUserbyId();  
+    
+  }
+
+  handleCoupon():any{   
+    
+    this.checkoutService.getbyCouponCode (this.couponRedeemForm.value)
+     .subscribe((res:any)=>{
+       console.log('after redeem');         
+        this.discountObj=res; 
+        console.log(res)  ;       
+        this.orderObj.couponId=this.discountObj[0].couponId;          
+       
+     });             
+}
+
+ 
+  handleCheckout():any{
+    console.log(this.orderObj);
+    this.checkoutService.addOrder(this.orderObj)
+    .subscribe((res:any)=>{
+      console.log(res);
+      alert('Thank you for shopping');
+    });
+  }
+
+
+  getUserbyId():any{
+    this.checkoutService.getUserByUserId(this.currentUser)
+    .subscribe((res:any)=>{
+      console.log('userIdcomponent');
+      console.log(res); 
+      this.loggedInUser=res;
+    })   
+  } 
+
+  handleUpdateUser():any{
+    //console.log(this.updateAddressform.value);
+    this.newAddress=this.updateAddressform.value;    
+    this.loggedInUser.userAddress=this.newAddress.userAddress;
+    //console.log(this.loggedInUser)
+    this.checkoutService.updateUserAddress(this.loggedInUser)
+     .subscribe((res:any)=>{
+       alert('New Address updated')
+       console.log(res);             
+     });       
   }
 
 }
--- src/app/shopping/services/cart.service.ts
+++ src/app/shopping/services/cart.service.ts
@@ -6,8 +6,43 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class CartService {
+  
 
   constructor(private http: HttpClient) { }
+
+  getbyCouponCode(couponcode:any):any{
+    console.log(couponcode.couponCode);
+    return this.http.get(`https://localhost:44346/api/Discount/?p_couponCode=${couponcode.couponCode}`)
+     .pipe(map((res:any)=>{
+       return res;
+     }))
+  }
+
+  updateUserAddress(loggedInUser:any):any{    
+    return this.http.put(`https://localhost:44346/api/Users`,loggedInUser)
+     .pipe(map((res:any)=>{
+       console.log('cartservices.ts');
+       console.log(res);
+       return res;
+     }))
+  }
+
+  addOrder(orderObj:any):any{
+    return this.http.post(`https://localhost:44346/api/Order`,orderObj)
+     .pipe(map((res:any)=>{
+       return res;
+     }))
+  }
+
+  getUserByUserId(userId:string):any{
+    return this.http.get(`https://localhost:44346/api/Users/?p_userId=${userId}`)
+    .pipe(map((res:any)=>{
+      console.log('user');
+      console.log(res);
+      
+      return res;
+    }))
+  }
 
   getCartbyUserId(userId: string | null): any{ 
     let cartUrl = `https://localhost:44346/api/Cart?userId=${userId}`;
