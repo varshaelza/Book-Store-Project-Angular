9f3ffd3b262b60ce5dcdeb1e93f34c3ac2c10b97
varshaelza <varshaelza@gmail.com>
2021-11-01 12:26:00 -0400

    login updated

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -23,24 +23,27 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   handleLogin(formData:any){
+    this.isActive=true
+    this.isLoggedIn = true;
     console.log("logging in");
     console.log(formData.value);
     this.authService.login(formData.value).subscribe((res:any)=>{ 
       console.log(res)
       
-      if(res&&res.isactive==false)
-      {
-        this.isActive=false
-        this.isLoggedIn = false;
-      }
-      else if(res && res.userID){
+      if(res && res.userID&&res.isactive){
         localStorage.setItem('authToken', res.userID);
         this.authService.adminstatus.next(res.isAdmin);
         //read query parameter to now return url
         this.isLoggedIn = true;
+        this.isActive=true
         this.router.navigateByUrl(this.activeRoute.snapshot.queryParams['returnURL']);
       }
-     
+      else if(res&&res.userID&&res.isactive==false)
+      {
+        this.isActive=false
+        this.isLoggedIn = false;
+        this.loginForm.setValue({username: '', password: ''});
+      }
       else{
         
         this.isLoggedIn = false;
